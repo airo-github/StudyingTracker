a011f96bc6f04c8579da93b7032f357d0f610167
Onotatsu <125164869+airo-github@users.noreply.github.com>
2023-06-15 14:46:08 +0900

    add: ログインページとヘッダー（仮）作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users, only: %i[new create]
 end
