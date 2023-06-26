5e2d508412c81541d2838decf8e884ae369a910b
satou haruka <haruka.satou.37@icloud.com>
2023-06-26 15:57:18 +0900

    userdestroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:notice] = 'ユーザーを削除しました。'
+    redirect_to :root #削除に成功すればrootページに戻る
+  end
+
   private
 
   def user_params
