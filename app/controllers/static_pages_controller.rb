f68a3d3b4755e58272dcf602e08160240dcdcb32
satou haruka <haruka.satou.37@icloud.com>
2023-06-28 17:15:38 +0900

    policy

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,3 +1,7 @@
 class StaticPagesController < ApplicationController
   def top; end
+
+  def privacy; end
+
+  def service; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
+  get 'privacy', to: 'static_pages#privacy'
+  get 'service', to: 'static_pages#service'
   resources :users, only: %i[new create destroy]
   resource :profile, only: %i[show edit update]
   resources :study_times, only: %i[new create index update destroy] do
