02084ecdb0c29bdc180edf8b66a930cf57368d27
yamashita-takahiro-44 <shimolen@gmail.com>
2023-06-28 19:48:40 +0900

    MVP

--- app/models/study_time.rb
+++ app/models/study_time.rb
@@ -2,13 +2,4 @@ class StudyTime < ApplicationRecord
   belongs_to :user, class_name: 'User'
 
   validates :user_id, presence: true
-  validate :validate_unique_status_zero
-
-  private
-
-  def validate_unique_status_zero
-    if self.status == 0 && StudyTime.exists?(user_id: self.user_id, status: 0)
-      errors.add(:status, "already exists with status 0")
-    end
-  end
 end
