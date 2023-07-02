547c6ca622be9c4875758b1a81c215c919810606
Okuma Ryoya <kumaryoya05210521@yahoo.co.jp>
2023-07-02 21:48:02 +0900

    fix_study_times_bug

--- app/models/study_time.rb
+++ app/models/study_time.rb
@@ -2,4 +2,5 @@ class StudyTime < ApplicationRecord
   belongs_to :user, class_name: 'User'
 
   validates :user_id, presence: true
+  validates :status, uniqueness: { scope: :user_id}, if: -> { status == 0 }
 end
